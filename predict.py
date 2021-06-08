b8c6076583d61e868f716dd76cdc713424be0dfa
Ajay <65583605+ajaynair1998@users.noreply.github.com>
2021-06-09 03:08:17 +0530

    Add files via upload

--- predict.py
+++ predict.py
@@ -1,13 +1,12 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import MaxPooling2D, Conv2D, GlobalAveragePooling2D, ReLU, Dropout, Dense, Softmax
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img, array_to_img
+from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing import image
 import os
 import numpy as np
 
-# relative ppath to data r'' is raw string
+
 predict_folder = r'predict'
 img = r'image.jpg'
 saved_model = r'saved_model'
@@ -15,12 +14,7 @@ saved_model = r'saved_model'
 # functions for accessing directory path
 __dirname = os.path.dirname(os.path.realpath(__file__))
 image_path = os.path.join(__dirname, predict_folder, img)
-# print(__dirname, '<------------->', os.path.realpath(__file__))
-# print(os.path.join(__dirname, train_path_positive))
 
-# initialisers for each layer
-# layer1_bias=
-# layer1_weights=
 
 
 img = load_img(image_path, target_size=(512, 512),
@@ -30,7 +24,7 @@ img = load_img(image_path, target_size=(512, 512),
 img_array = image.img_to_array(img)
 img_array = np.expand_dims(img_array, axis=0)
 
-# havent found out how to set initial bias yet
+
 model = Sequential([
     Conv2D(256, input_shape=(512, 512, 1), kernel_size=(2, 2),
            strides=(2, 2), padding='same', activation=tf.keras.activations.sigmoid),
