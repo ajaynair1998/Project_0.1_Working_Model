ffd50d0c753345953e3822050e4d103ad64199dc
Ajay <65583605+ajaynair1998@users.noreply.github.com>
2021-06-09 18:25:51 +0530

    Add files via upload

--- predict.py
+++ predict.py
@@ -1,30 +1,29 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import MaxPooling2D, Conv2D, GlobalAveragePooling2D, ReLU, Dropout, Dense, Softmax
-from tensorflow.keras.preprocessing.image import load_img
-from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
-import numpy as np
 
-
+# raw strings
 predict_folder = r'predict'
 img = r'image.jpg'
 saved_model = r'saved_model'
 
 # functions for accessing directory path
 __dirname = os.path.dirname(os.path.realpath(__file__))
-image_path = os.path.join(__dirname, predict_folder, img)
+image_path = os.path.join(__dirname, predict_folder)
 
+# Use image Data Generator to Take in Image to Predict
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+test_generator = test_datagen.flow_from_directory(
+    image_path,
+    target_size=(512, 512),
+    batch_size=1,
+    color_mode='grayscale',
+    class_mode='binary',
+    shuffle=False)
 
-
-img = load_img(image_path, target_size=(512, 512),
-               color_mode='grayscale',
-               )
-
-img_array = image.img_to_array(img)
-img_array = np.expand_dims(img_array, axis=0)
-
-
+# Predefined Layers of the Model
 model = Sequential([
     Conv2D(256, input_shape=(512, 512, 1), kernel_size=(2, 2),
            strides=(2, 2), padding='same', activation=tf.keras.activations.sigmoid),
@@ -44,11 +43,15 @@ model = Sequential([
 
 ])
 
+# Print the summary of the model
 model.summary()
 
+# Load the optimised weights
 model.load_weights(os.path.join(__dirname, saved_model, 'my_model.h5'))
 
-prediction = model.predict(img_array)
+# Predict from generator (returns probabilities)
+prediction = model.predict(test_generator, steps=len(test_generator), verbose=1)
 
+# Print the probability using a single image
 print("The Probability of the Image being Negative is \t" + str(prediction[0][0] * 100) + '\nand Positive is\t' + str(
     prediction[0][1] * 100))
